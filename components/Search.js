999821a26486a553ef093dfe5a002f00ab9570c6
HuGuetteRekt <37801447+HuGuetteRekt@users.noreply.github.com>
2020-03-16 16:44:52 +0100

    Add files via upload

---
+++ components/Search.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StyleSheet, View, TextInput, Button } from 'react-native'
+
+class Search extends React.Component {
+    render(){
+        return(
+            <View style={{ marginTop: 20, backgroundColor : 'red' }}>
+                <TextInput style= {styles.textinput} placeHolder='Titre du film'/>
+                <Button title='Rechercher' onPress={() =>{}}/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    main_container: {
+        flex:1,
+        marginTop: 20
+    },
+    textinput: {
+        marginLeft:5,
+        marginRight :5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
+    }
+})
+
+export default Search
